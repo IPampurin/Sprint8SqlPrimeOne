0441f247d5f87c515271463f6e173fbf8afebde9
IPampurin <i.pampurin@yandex.ru>
2025-01-26 16:40:11 +0500

    first save

--- selectQuery.go
+++ selectQuery.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
 
-    _ "modernc.org/sqlite"
+	_ "modernc.org/sqlite"
 )
 
 func main() {
-    db, err := sql.Open("sqlite", "db_name.db")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer db.Close()
-} 
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	var (
+		product string
+		price   int
+	)
+	row := db.QueryRow("SELECT product, price FROM products WHERE id = :id", sql.Named("id", 1))
+	err = row.Scan(&product, &price)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(product, price)
+}
